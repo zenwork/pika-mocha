9a109f2f15a78d991107539cb043d0b62370cf70
flo <flo@noumenadigital.com>
2019-10-20 12:55:30 +0200

    adding init script

--- init.sh
+++ init.sh
@@ -3,14 +3,18 @@ set -o nounset
 set -o pipefail
 
 target=${1:-"web-project"}
-
-wget --no-check-certificate https://github.com/zenwork/pika-mocha/archive/master.zip
-unzip master.zip
-rm -rf master.zip
+archive=/tmp/pika-mocha.zip
+echo "downloading"
+wget --no-check-certificate --quiet --output-document $archive https://github.com/zenwork/pika-mocha/archive/master.zip
+echo "unpacking"
+unzip -oq -d . $archive
+rm -rf $archive
 rm -rf pika-mocha-master/.git
 rm -rf pika-mocha-master/README.md
+echo "preparing $target"
 sed -i -e "s/pika-mocha/$target/" pika-mocha-master/package.json
 sed -i -e "s/^.*author.*$//" pika-mocha-master/package.json
 sed -i -e '/^\s*$/d' pika-mocha-master/package.json
 mv pika-mocha-master "$target"
+echo "done"
 
